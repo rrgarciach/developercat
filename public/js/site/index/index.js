2f572219305f72dab8b930afaa124e1d61cbae77
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-18 00:59:02 -0500

    Adjusted carrousel.

--- public/js/site/index/index.js
+++ public/js/site/index/index.js
@@ -1,3 +1,7 @@
 'use strict';
 
 $('.carousel.carousel-slider').carousel({full_width: true});
+
+setInterval(() => {
+	$('.carousel').carousel('next');
+}, 6000);
--- routes/views/index.js
+++ routes/views/index.js
@@ -8,7 +8,7 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+
 	locals.whatIDo = [
 		{
 			title: 'Product Definition',
@@ -52,9 +52,9 @@ exports = module.exports = function (req, res) {
 					stack: 'Related technologies: Node.js, express.js, Ionic Framework, AngularJS, Arduino',
 				},
 				{
-					name: 'Jupiter API',
-					description: `REST API platform to serve multiple mobile and hardware clients to be integrated as an enterprise ERP system.`,
-					stack: 'Related technologies: Node.js, express.js, Ionic Framework, AngularJS, Arduino',
+					name: 'Greenhousify IoT',
+					description: `IoT project to monitor and control domestic greenhouses.`,
+					stack: 'Related technologies: Node.js, MQTT, Arduino, Ionic Framework',
 				},
 			]
 		},
@@ -62,9 +62,14 @@ exports = module.exports = function (req, res) {
 			name: 'In Progress',
 			items: [
 				{
-					name: 'API',
-					description: `REST API platform to serve multiple mobile and hardware clients to be integrated as an enterprise ERP system.`,
-					stack: 'Related technologies: Node.js, express.js, Ionic Framework, AngularJS, Arduino',
+					name: 'GC Orders',
+					description: `Platform and mobile application that enables sales department to easily capture and centralize orders from customers through a REST API platform.`,
+					stack: 'Related technologies: Node.js, express.js, Ionic Framework, AngularJS, Heroku.',
+				},
+				{
+					name: 'GC Orders Mobile',
+					description: `Hybrid mobile application to perform as client of GC Orders.`,
+					stack: 'Related technologies: Ionic Framework, ES6',
 				},
 			]
 		},
@@ -72,9 +77,29 @@ exports = module.exports = function (req, res) {
 			name: 'Done',
 			items: [
 				{
-					name: 'Jupiter API',
-					description: `REST API platform to serve multiple mobile and hardware clients to be integrated as an enterprise ERP system.`,
-					stack: 'Related technologies: Node.js, express.js, Ionic Framework, AngularJS, Arduino',
+					name: 'Cosmos IoT Prototype',
+					description: `IoT end-to-end system for home/office automation, supervision and data acquisition of devices such as HVAC controls, lights and access control using MQTT protocol, Web/Mobile applications as integrating layer.`,
+					stack: 'Related technologies: Node.js, Ionic Framework, Bluetooth, Raspberry Pi, Arduino.',
+				},
+				{
+					name: 'Ritty',
+					description: `Web enterprise application based on Node.js with expressJS and AngularJS as a Single Page Application with Angular Material Design to fit Google’s Material Design guidelines. In this project, backend’s business logic is served as RESTful API for Software As A Service architecture.`,
+					stack: 'Related technologies: JavaScript, AngularJS, Angular Material Design, Node.js, Express.js, mocha.js, MySQL, Stylus, Grunt, Bower, npm.',
+				},
+				{
+					name: 'Mantis UI v3',
+					description: `New version from scratch. This new brand version was developed using Angular 2 with TypeScript to meet business logic requirements`,
+					stack: 'Related technologies: Angular2, AngularJS, Node.js, TypeScript, RabbitMQ.',
+				},
+				{
+					name: 'Mantis UI v2',
+					description: `Platform application based in a legacy code using raw PHP Zend Framework. A major refactor work was required as a Single Page Application with a REST API backend using AngularJS and NodeJS`,
+					stack: 'Related technologies: AngularJS, Node.js, PHP, Zend Framework, jQuery UI.',
+				},
+				{
+					name: 'Vivint Skydome simulator',
+					description: `Participating as software engineer in development of mobile application simulator of domotics/surveillance panel (for Android and iOS). The application has been developed as a simulator/demo to introduce new customers with the real device UI and functionalities.`,
+					stack: 'Related technologies: Apache Cordova, JavaScript, jQuery UI, Android/iOS SDK.',
 				},
 			]
 		},
