9e22498c604d4ca426c72363bdd11670469c3f60
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-17 21:16:21 -0500

    Fixed alignment.

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,29 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	
+	locals.whatIDo = [
+		{
+			title: 'Product Definition',
+			image: 'product-definition.jpg',
+			description: `Defining`
+		},
+		{
+			title: 'Technical Management',
+			image: 'technical-management.jpg',
+			description: `Defining`
+		},
+		{
+			title: 'Software Architecture/Design',
+			image: 'architectural-design.jpg',
+			description: `Defining`
+		},
+		{
+			title: 'Software Development',
+			image: 'software-development.jpg',
+			description: `Defining`
+		},
+	];
 
 	// Render the view
 	view.render('index');
